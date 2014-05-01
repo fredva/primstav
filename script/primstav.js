647bbc70032bff25c527e6a27c026d6bd6111db3
Fredrik Valdmanis <fredrik@valdmanis.com>
2014-05-01 18:24:01 +0200

    Summer/winter toggle buttons. Closes #11, #8

---
+++ script/primstav.js
@@ -0,0 +1,7 @@
+document.querySelectorAll(".activate-summer")[0].onclick = function() {
+  document.getElementsByTagName("body")[0].className="show-summer";
+}
+
+document.querySelectorAll(".activate-winter")[0].onclick = function() {
+  document.getElementsByTagName("body")[0].className="show-winter";
+}
